af4aa09cac6552434f0167fcefd93c09d9acd9d0
anwarmoazam <anwar.palimarwar@gmail.com>
2022-12-06 17:37:50 +0530

    event listener on button container

--- 02-counter/counter.js
+++ 02-counter/counter.js
@@ -1,7 +1,9 @@
 let count = 0;
 
-const btns = document.querySelectorAll('.btn');
-const value = document.getElementById('value');
+/*
+
+let btns = document.querySelectorAll('.btn');
+let value = document.getElementById('value');
 
 btns.forEach(function(btn){
     btn.addEventListener('click',function(e){
@@ -24,4 +26,31 @@ btns.forEach(function(btn){
         }
         value.textContent = count;
     })
+})
+
+*/
+
+const btnContainer = document.querySelector('.button-container');
+const value = document.getElementById('value');
+
+btnContainer.addEventListener('click',function(e){
+    const styles = e.target.classList;
+    if(styles.contains('increase')){
+        count++;
+    } else if(styles.contains('decrease')){
+        count--;
+    } else{
+        count =0;
+    }
+
+    if(count > 0){
+        value.style.color = 'green';
+    }
+    if(count < 0){
+        value.style.color = 'red';
+    }
+    if(count === 0){
+        value.style.color = '#222'
+    }
+    value.textContent = count;
 })
