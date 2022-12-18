18d17f4284e59937070c0ee85f073ebe9f5892ba
anwarmoazam <anwar.palimarwar@gmail.com>
2022-12-19 01:14:44 +0530

    project scroll

--- 10-Scroll/js/app.js
+++ 10-Scroll/js/app.js
@@ -1,9 +1,70 @@
+const date = document.getElementById('date');
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
-const date = document.getElementById('date');
+const links = document.querySelector('.links');
+
+// set date
+date.innerHTML = new Date().getFullYear();
+
 
 navToggle.addEventListener('click',function(){
-    linksContainer.classList.toggle('show-links');
+    // linksContainer.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`
+    } else{
+        linksContainer.style.height = 0;
+    }
 });
 
-date.innerHTML = new Date().getFullYear();
+const navBar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
+
+// fixed navbars
+
+window.addEventListener('scroll',function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navBar.classList.add('fixed-nav');
+    } else{
+        navBar.classList.remove('fixed-nav');
+    }
+
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    } else{
+        topLink.classList.remove('show-link');
+    }
+});
+
+// select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link){
+    link.addEventListener('click',function(e){
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        // calculate the height
+        const navHeight = navBar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navBar.classList.contains('fixed-nav');
+        let position = element.offsetTop - navHeight;
+        if(!fixedNav){
+            position = position - navHeight;
+        }
+        if(navHeight > 82){
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left: 0,
+            top: position
+        });
+        linksContainer.style.height = 0;
+    })
+})
