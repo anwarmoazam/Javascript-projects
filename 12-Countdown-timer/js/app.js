c5ef425a24349627f569689fdb9bcda4e53c1a99
anwarmoazam <anwar.palimarwar@gmail.com>
2023-01-02 16:44:51 +0530

    project countdown-timer

---
+++ 12-Countdown-timer/js/app.js
@@ -0,0 +1,45 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+const futureDate = new Date(2023, 1, 1, 17, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+
+
+
+// Giveaway Ends On Sunday, 24 April 2020, 8:00am
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;
