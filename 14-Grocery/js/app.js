47db4aff0dbb428bb28b5cc3c60bf330db93a262
anwarmoazam <anwar.palimarwar@gmail.com>
2023-01-05 18:35:27 +0530

    project

--- 14-Grocery/js/app.js
+++ 14-Grocery/js/app.js
@@ -1,11 +1,40 @@
-const item = document.getElementById('grocery');
+// Select Items
+const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-item.addEventListener('input',function(e){
-    console.log(item.value);
-})
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit',addItem)
+
+function addItem(e){
+    const value = grocery.value;
     e.preventDefault();
-    console.log(item.value);
-})
+    if(value && !editFlag){
+        console.log('add item to the list');
+    }
+    else if(value && editFlag){
+        console.log('editing');
+    }
+    else{
+        displayAlert('please enter value','danger');
+    }
+}
+
+// display alert
+function displayAlert(text,action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    // remove alert
+    setTimeout(function(){
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    },1000)
+}
